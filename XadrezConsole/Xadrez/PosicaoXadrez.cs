cd2c66b4ec8af4defb6c6fb09c988d4a221bada6
Dionmax Nobre <dionmax@edu.univali.br>
2019-07-13 15:45:53 -0300

    Adicionando posições do Xadrez

---
+++ XadrezConsole/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,23 @@
+﻿using XadrezConsole.EntitiesTabuleiro;
+
+namespace XadrezConsole.Xadrez
+{
+    class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao ToPosition()
+            => new Posicao(8 - Linha, Coluna - 'a');
+
+        public override string ToString()
+            => "" + Coluna + Linha;
+    }
+}
